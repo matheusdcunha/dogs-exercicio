6e7ba1c7d351e1573799221ab132388821a4ea43
Matheus da Cunha da Fonseca <matheus.cunha.fonseca50@gmail.com>
2024-12-05 12:10:41 -0300

    Adicionado a parte de criar usuário

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, Navigate, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginPasswordLost";
@@ -10,7 +10,8 @@ import styles from "./Login.module.css";
 const Login = () => {
   const { login } = useContext(UserContext);
 
-  if (login === true) return <Navigate to="/conta" />;
+  if (login === true) return;
+  <Navigate to="/conta" />;
   return (
     <section className={styles.login}>
       <div className={styles.forms}>
--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -5,6 +5,10 @@ const types = {
     regex: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
     message: "Preencha um email válido",
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message: "A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.",
+  },
 };
 
 const useForm = (type) => {
