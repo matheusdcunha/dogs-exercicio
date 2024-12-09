9750952438ec6c6f67790919cbc719d5009651b7
Matheus da Cunha da Fonseca <matheus.cunha.fonseca50@gmail.com>
2024-12-09 10:40:30 -0300

    Adicionei estilo ao deletar a foto

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import PhotoComments from "./PhotoComments";
 import styles from "./PhotoContent.module.css";
+import { UserContext } from "../../UserContext";
+import PhotoDelete from "./PhotoDelete";
 
 const PhotoContent = ({ data }) => {
+  const user = useContext(UserContext);
   const { photo, comments } = data;
 
   return (
@@ -14,7 +17,8 @@ const PhotoContent = ({ data }) => {
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? <PhotoDelete id={photo.id} /> : <Link to={`/perfil${photo.author}`}>@{photo.author}</Link>}
+
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className="title">
@@ -26,7 +30,7 @@ const PhotoContent = ({ data }) => {
           </ul>
         </div>
       </div>
-      <PhotoComments id={photo.id} comments={comments}/>
+      <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
 };
