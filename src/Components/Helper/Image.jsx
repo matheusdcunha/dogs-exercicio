de72ce02c951def67aeb8fad7ba137dab11ec342
Matheus da Cunha da Fonseca <matheus.cunha.fonseca50@gmail.com>
2024-12-09 11:27:42 -0300

    Adicionei a animação do osso ao loading

--- src/Components/Helper/Image.jsx
+++ src/Components/Helper/Image.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import styles from "./Image.module.css";
 
-const Image = ({alt, ...props}) => {
-
+const Image = ({ alt, ...props }) => {
   const [skeleton, setSkeleton] = useState(true);
 
-  function handleLoad({target}){
+  function handleLoad({ target }) {
     setSkeleton(false);
-    target.style.opacity= 1;
+    target.style.opacity = 1;
   }
 
   return (
     <div className={styles.wrapper}>
       {skeleton && <div className={styles.skeleton}></div>}
-      <img onLoad={handleLoad} alt={alt} className={styles.img} {...props}/>
+      <img onLoad={handleLoad} alt={alt} className={styles.img} {...props} />
     </div>
-    )
+  );
 };
 
 export default Image;
